118ffa54acd96f3070c1d071a7d174ae7143061c
Davin Song <53934996+daxjective@users.noreply.github.com>
2024-05-29 15:37:22 +0900

    Image Component

--- app/list/page.js
+++ app/list/page.js
@@ -1,14 +1,21 @@
+import Image from "next/image";
+import 작명 from '/public/food0.png'
+// import 작명 from '@/public/food0.png'
+
 export default function List() {
   let 상품 = ["Tomatoes", "Pasta", "Coconut"];
 
   return (
     <div>
       <h2 className="title">Products</h2>
-      {상품.map((v, i) => {
+      {상품.map((v,i) => {
         return (
-          <div className="food">
+          <div className="food" key={i}>
+            {/* <img src="/food0.png" food-img></img> */}
+            <Image src={작명} className="food-img"></Image>
+
             <h4>{v} $16</h4>
-          </div>
+          </div> 
         );
       })}
     </div>
