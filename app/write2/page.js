13d17785b5d4947a3688962629a1079f401ba53c
Davin Song <53934996+daxjective@users.noreply.github.com>
2024-05-31 10:43:42 +0900

    Client Component 환경에서 Server Actions 사용

--- app/write2/page.js
+++ app/write2/page.js
@@ -1,9 +1,14 @@
+"use client";
+
+/** Client에서 Server Actions 사용하는 방법
+ * - 서버 API 만드는 것이랑 복잡도가 같다. => 굳이 ?
+ * - 장점 : 한 곳에서 관리 할 수 있다.
+ */
+import { handleSubmit } from "./action";
+
 export default async function write2() {
-  // Server API
-  async function handleSubmit(formData) {
-    "use server";
-    console.log(formData.get("title"));
-  }
+  const db = (await connectDB).db("forum");
+  await db.collection;
 
   return (
     <div>
